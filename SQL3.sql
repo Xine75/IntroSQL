b4fac447d35fc1d0b1cc625a56f6676d00fb37f5
Xine <cjdoza@gmail.com>
2021-03-09 15:40:07 -0600

    initial commit

---
+++ SQL3.sql
@@ -0,0 +1,5 @@
+﻿INSERT INTO Artist (ArtistName, YearEstablished) VALUES ('Janet Jackson', 1981);
+
+INSERT INTO Album (Title, ReleaseDate, AlbumLength, Label, ArtistId, GenreId) VALUES ('Rhythym Nation', '09/19/1989', 3718, 'A&M', 28, 7);
+
+INSERT INTO Song (Title, SongLength, ReleaseDate, GenreId, ArtistId, AlbumId) VALUES ('Miss You Much', 851, '09/19/1989', 7, 28, 23);
---
+++ SQLQuery2.sql
@@ -0,0 +1,44 @@
+﻿
+SELECT
+    Id,
+    Label
+FROM Genre;
+
+SELECT 
+Id,
+ArtistName,
+YearEstablished
+FROM Artist
+ORDER BY ArtistName;
+
+SELECT  s.Title,
+        a.ArtistName
+FROM Song s
+JOIN Artist a on s.ArtistId=a.id;
+
+Select	DISTINCT
+	    a.ArtistName
+FROM	Album
+JOIN	Artist a on ArtistId = a.id
+JOIN	Genre g on GenreId = g.id
+WHERE	g.id = 7;
+
+Select  DISTINCT
+        a.ArtistName
+FROM    Album
+JOIN    Artist a on ArtistId = a.id
+JOIN    Genre g on GenreId = g.id
+WHERE   g.id = 2 OR g.id = 4;
+
+Select  Title
+FROM    Album 
+WHERE   AlbumLength IS NULL;
+
+select *
+from Album
+
+select *
+from Song
+        
+
+
